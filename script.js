e58e884fd3fc4b45699db8112faf4bb11ec5dcc2
GADOBUA <emmanuelobua54@gmail.com>
2024-11-19 10:37:03 +0300

    add media screen for the 780px

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let lastHighlight = document.getElementById(lastPopup);
-lastHighlight.style.border= "none";
+// let firstNumber = parseInt(prompt("Please input the first number"));
+// let secondNumber =  parseInt(prompt("Please input the second number"));
+// let final = prompt("what case");
+// switch(final){
+//     case 1:
+//         final = firstNumber + secondNumber;
+//         console.log("The sum is" + final);
+//         break;
+
+//         case 2:
+//             final = firstNumber - secondNumber;
+//             console.log("the difference is" + final)
+//             break
+// }
